1ecd071bd004ed06deba5a3b32c4468ef15b40d5
Eelis Rossi <eelis.rossi@inmicsnebula.fi>
2023-05-26 19:00:19 +0300

    parsing words works now

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 use std::{env, error::Error, ffi::OsString, fs::File, process};
 
-fn run() -> Result<(), Box<dyn Error>> {
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
-
 fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
     match env::args_os().nth(1) {
         None => Err(From::from("expected 1 argument, but got none")),
@@ -19,8 +8,26 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
 }
 
 fn main() {
-    if let Err(err) = run() {
+    if let Err(err) = get_words() {
         println!("{}", err);
         process::exit(1);
     }
+}
+
+fn get_words() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').from_reader(file);
+
+    let mut words: Vec<String> = Vec::new();
+
+    for result in rdr.records() {
+        let record = result?;
+        let word = &record[0];
+        words.push(word.to_string());
+    }
+
+    println!("{:?}", words);
+
+    Ok(())
 }
